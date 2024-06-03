bd51d84188ba60bf8f2896ff03c857b4ad8a8784
MrBabon <christophedanna06@gmail.com>
2024-06-04 00:35:22 +0200

    mode dark en cours

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+import { setupDarkMode } from './dark-mode.js';
+
+// DARK MODE    
+document.addEventListener('DOMContentLoaded', setupDarkMode);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'media',
+  darkMode: 'selector',
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
@@ -12,8 +12,11 @@ module.exports = {
         'deepNavy': '#051a24',
         'tealBlue': '#06373A',
         'deepTeal': '#1F5F5B',
+        'emeraldGreen': '#fff700',
         'emeraldGreen': '#159947',
-        'neonGreen': '#4FF57B'
+        'neonGreen': '#4FF57B',
+        'moon': '#183153',
+        'sun': '#73C0FC'
       },
       gridTemplateColumns:{
         "menu": "300px 1fr 150px"
@@ -22,6 +25,15 @@ module.exports = {
         sm: '0 1px 2px var(0,0,0,0.2)',
         DEFAULT: '1px 1px 3px var(0,0,0,0.2)',
         lg: '0 8px 16px var(0,0,0,0.2)',
+      },
+      animation: {
+        spin: 'spin 15s linear infinite',
+      },
+      keyframes: {
+        spin: {
+          from: { transform: 'rotate(0deg)' },
+          to: { transform: 'rotate(360deg)' },
+        },
       },
     },
   },
