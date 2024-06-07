d73e0a2917fa8c8f2b6647fc81248deb7d390dba
MrBabon <christophedanna06@gmail.com>
2024-06-07 17:13:56 +0200

    page projet en cours

--- assets/js/dark-mode-projet.js
+++ assets/js/dark-mode-projet.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('toggle-dark-mode');
     const modeIcon = document.getElementById('mode-icon');
-    
+
     toggleButton.addEventListener('click', (event) => {
         event.preventDefault();
         document.documentElement.classList.toggle('dark');
+
         if (document.documentElement.classList.contains('dark')) {
-            modeIcon.src = './assets/images/icon/moon.svg';
+            modeIcon.src = '../assets/images/icon/moon.svg';
             modeIcon.classList.remove('animate-spin');  
         } else {
-            modeIcon.src = './assets/images/icon/sun.svg';
+            modeIcon.src = '../assets/images/icon/sun.svg';
             modeIcon.classList.add('animate-spin');
         }
-    })
-})
+    });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-import { setupDarkMode } from './dark-mode.js';
 import { initializeMenu, initializePhoneMenu, initializeDropdownMenu } from './navbar.js';
 import { setupScrollTop } from './scroll-top.js';
 import { setupLoader } from './loader.js';
 import { setupLightEffect } from './light-effect.js';
 
-// DARK MODE    
-document.addEventListener('DOMContentLoaded', setupDarkMode);
+
 
 // Navbar
 document.addEventListener("DOMContentLoaded", function () {
